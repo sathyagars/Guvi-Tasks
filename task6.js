7ef55d857f4c0350ebebd7d1dfbac24675d1c8cd
sathyagars <sathyagarsakthivel@gmail.com>
2024-06-03 16:08:01 +0530

    second commit

---
+++ task6.js
@@ -0,0 +1,55 @@
+let myResume={
+    "basics": {
+      "name": "SATHYAGAR S",
+      "email": "sathyagarsakthivel@gamil.com",
+      "phone": 9994010261,
+      "degree": "B.sc IT",
+      "location": {
+        "address": "2/46, unjavelampatty, pollachi",
+        "postalCode": 642003,
+        "city": "coimbatore",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "github":"https://github.com/sathyagars"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "indegene",
+        "position": "Web developer",
+        "startDate": "2022-11-04",
+        "endDate": "2021-08-15",
+        
+      },
+    ],
+    "education": [
+      {
+        "institution": "Nehru arts and science college",
+        "department": "B.sc IT",
+        "studyType": "fulltime",
+        "batch start year": 2016,
+        "batch end year": 2019,
+      }
+    ],
+    "skills": [
+      {
+        "name": "Html,javascript",
+        "level": "beginer",
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "script writter,youtuber,",
+      }
+    ]
+  }
+  console.log(myResume);
